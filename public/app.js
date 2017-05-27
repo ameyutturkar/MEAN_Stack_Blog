89214b1009a1a93b54be346e6d198a9d3a5ef984
ameyutturkar <au.utturkar@gmail.com>
2017-05-27 17:54:46 -0400

    working on mongoose integration, video at 30 min mark

--- public/app.js
+++ public/app.js
@@ -3,5 +3,16 @@
  */
 (function () {
     angular //angular global variable
-        .module("BlogApp", []);
-})
+        .module("BlogApp", [])
+        .controller("BlogController", BlogController); //this can be written in separate file to follow best practices
+
+
+    function BlogController($scope, $http) { //scope interface service allows to interact with html templates
+        $scope.createPost = createPost; //listen to createPost event handler
+
+        function createPost(post) { //tie above to the function of the same name locally
+            console.log(post);
+            $http.post("/api/blogpost", post); //by convention /api or /rest is used for dynamic content
+        }
+    }
+})();
