b854e3529baf5f8bfcedf5ecb8b59392f807f303
ameyutturkar <au.utturkar@gmail.com>
2017-05-27 15:22:14 -0400

    added some angular and http req and res

--- server.js
+++ server.js
@@ -7,4 +7,10 @@ var app = express(); // creates instance of express
 
 app.use(express.static(__dirname + '/public')); //all static content is going to be in the public folder in the current dir
 
+app.post("/api/blogpost", createPost); //to listen to incoming requests, //map it to a local function to handle requests
+
+function createPost() {
+    console.log("hello from server createPost()");
+}
+
 app.listen(3000); //to listen to incoming requests, 3000 is a very common port for dev
